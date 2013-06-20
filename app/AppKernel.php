09e70b0b8ccacf4fe0032107e174eb7c7393df10
Boris Schapira <bschapira@clever-age.com>
2013-06-20 15:10:58 +0200

    Sortie de MopaBoostrapBundle, Ajout de Boostrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,8 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
-
             new Sch\WlBundle\SchWlBundle(),
         );
 
