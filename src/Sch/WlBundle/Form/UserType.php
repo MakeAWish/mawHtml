3df2cd4b0b068f8f3641e11a64bde30dd8515b1d
Boris Schapira <bschapira@clever-age.com>
2013-06-20 17:29:44 +0200

    Formulaire User
    app/console doctrine:generate:form SchWlBundle:User

---
+++ src/Sch/WlBundle/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Sch\WlBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('salt')
+            ->add('email')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('birthdate')
+            ->add('roles')
+            ->add('circle')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sch\WlBundle\Entity\User'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'sch_wlbundle_usertype';
+    }
+}
