57c796b23eaa00a23d57ad74b941d163cf2312ab
Boris Schapira <bschapira@clever-age.com>
2013-06-25 17:57:32 +0200

    Gestion des voeux (CRUD simple)

--- src/Sch/WlBundle/Controller/WishController.php
+++ src/Sch/WlBundle/Controller/WishController.php
@@ -2,18 +2,208 @@
 
 namespace Sch\WlBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sch\WlBundle\Entity\Wish;
+use Sch\WlBundle\Form\WishType;
 
+/**
+ * Wish controller.
+ *
+ * @Route("/wish")
+ */
 class WishController extends Controller
 {
+
     /**
-     * @Route("/wish")
+     * Lists all Wish entities.
+     *
+     * @Route("/")
+     * @Method("GET")
      * @Template()
      */
     public function indexAction()
     {
-        return array();
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('SchWlBundle:Wish')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+    /**
+     * Creates a new Wish entity.
+     *
+     * @Route("/")
+     * @Method("POST")
+     * @Template("SchWlBundle:Wish:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Wish();
+        $form = $this->createForm(new WishType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('sch_wl_wish_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Wish entity.
+     *
+     * @Route("/new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Wish();
+        $form   = $this->createForm(new WishType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Finds and displays a Wish entity.
+     *
+     * @Route("/{id}")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('SchWlBundle:Wish')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Wish entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Wish entity.
+     *
+     * @Route("/{id}/edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('SchWlBundle:Wish')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Wish entity.');
+        }
+
+        $editForm = $this->createForm(new WishType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Wish entity.
+     *
+     * @Route("/{id}")
+     * @Method("PUT")
+     * @Template("SchWlBundle:Wish:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('SchWlBundle:Wish')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Wish entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new WishType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('sch_wl_wish_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+    /**
+     * Deletes a Wish entity.
+     *
+     * @Route("/{id}")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('SchWlBundle:Wish')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Wish entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('sch_wl_wish_index'));
+    }
+
+    /**
+     * Creates a form to delete a Wish entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
     }
 }
---
+++ src/Sch/WlBundle/Form/WishType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sch\WlBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class WishType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sch\WlBundle\Entity\Wish'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'sch_wlbundle_wishtype';
+    }
+}
